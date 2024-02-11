6d49d139049c6ce1443dbd84da9bad16daef205c
beni-f <benifissha@gmail.com>
2024-02-11 17:43:40 +0300

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@
 from models import storage
 from api.v1.views import app_views
 from flask import Flask
+from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -15,4 +16,11 @@ def teardown(exception=None):
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
