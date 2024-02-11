8b5c598a34b1ee5a81edee7405967f87e1b43d1f
beni-f <benifissha@gmail.com>
2024-02-11 19:42:18 +0300

    initial commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 """
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from os import environ
 
 app = Flask(__name__)
@@ -14,6 +14,10 @@ app.register_blueprint(app_views)
 def teardown(exception=None):
     """ Close Storage """
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == '__main__':
     host = environ.get('HBNB_API_HOST')
