c3012f330f88a7edee3363ddd3d21f76d110c7ae
beni-f <benifissha@gmail.com>
2024-02-11 18:46:49 +0300

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,5 +22,5 @@ if __name__ == '__main__':
     if not host:
         host = '0.0.0.0'
     if not port:
-        port = '5500'
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
