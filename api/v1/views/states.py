1c3eafa157a2ddd888c30c7fce5049e10a78d127
beni-f <benifissha@gmail.com>
2024-02-12 22:22:18 +0300

    updated

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,7 +16,7 @@ def get_states():
     list_states = []
     for state in states:
             list_states.append(state.to_dict())
-    return list_states
+    return jsonify(list_states)
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state_by_id(state_id):
@@ -27,7 +27,7 @@ def get_state_by_id(state_id):
     if not state:
         abort(404)
     return jsonify(state.to_dict())
-
+                         
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """
@@ -39,7 +39,7 @@ def delete_state(state_id):
          abort(404)
     storage.delete(state)
     storage.save()
-    return jsonify({}), 200
+    return make_response(jsonify({}), 200)
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def add_state():
@@ -70,10 +70,10 @@ def update_state(state_id):
     if not data:
         abort(400, "Not a JSON")
 
-    ignore = ['id', 'created_at', 'updated_at']
+    ignore_keys = ['id', 'created_at', 'updated_at']
 
     for key, value in data.items():
-        if key not in ignore:
+        if key not in ignore_keys:
             setattr(state, key, value)
     storage.save()
     return make_response(jsonify(state.to_dict()), 200)
