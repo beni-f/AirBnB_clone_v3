c093c5f0b04d98b1d50f47c69549b22c60dfc6aa
beni-f <benifissha@gmail.com>
2024-02-14 04:50:45 +0300

    updateda

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -9,7 +9,7 @@ from models.user import User
 from flask import jsonify, make_response, request, abort
 from api.v1.views import app_views
 
-@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
 def get_places(city_id):
     """
         Retrieves the list of all Place objects
